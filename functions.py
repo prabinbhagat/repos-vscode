369ad3a5b085771d7c56c09cca2cfecb7d984b3a
Prabin Bhagat <prabin.bhagat@gmail.com>
2022-10-17 11:14:55 +0530

    Func

---
+++ functions.py
@@ -0,0 +1,12 @@
+print('Hello world welcome to black vamp tuts')
+# What if i had same print statement multiple times
+print('Hello world welcome to black vamp tuts')
+print('Hello world welcome to black vamp tuts')
+print('Hello world welcome to black vamp tuts')
+print('Hello world welcome to black vamp tuts')
+
+# Now i want to change the text black to Red , i will have to manually change it in all the lines.
+# Which is hectic, So, for this FUNCTIONS comes to rescue. Lests create a function for the same
+
+
+
